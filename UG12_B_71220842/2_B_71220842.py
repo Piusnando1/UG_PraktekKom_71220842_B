a001822ca954aa49cadd31756f149ec4dc579a74
Piusnando1 <118422563+Piusnando1@users.noreply.github.com>
2022-12-08 13:03:56 +0700

    Add files via upload

---
+++ UG12_B_71220842/2_B_71220842.py
@@ -0,0 +1,9 @@
+print("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
+lambang = str(input("Masukan operator matematika yang ingin Anda hitung: "))
+pertama = int(input("Mau penjumlahan berapa: "))
+kedua = int(input("Print berapa banyak: "))
+if lambang == 1:
+    print(pertama)
+    print(kedua)
+    while = 0
+    
